d095c9ac177508c6db4d2a869e0de01477923b25
BenRey <br@massa.net>
2025-03-26 16:36:05 +0700

    feat: update useBalanceRefresh hook to return balance after refresh
    - Enhanced useBalanceRefresh hook to return the current balance after invoking refreshBalance.
    - Ensured that the balance defaults to 0n if not available, improving reliability of balance updates.

--- front/src/hooks/useBalanceRefresh.ts
+++ front/src/hooks/useBalanceRefresh.ts
@@ -2,13 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useAccountStore } from "@massalabs/react-ui-kit";
 
 export const useBalanceRefresh = () => {
-  const { connectedAccount, refreshBalance } = useAccountStore();
+  const { connectedAccount, refreshBalance, balance } = useAccountStore();
 
   return useQuery({
     queryKey: ["balance"],
     queryFn: async () => {
       if (!connectedAccount) return null;
       await refreshBalance(false);
+      return balance || 0n;
     },
     refetchInterval: 5000,
     enabled: !!connectedAccount,
