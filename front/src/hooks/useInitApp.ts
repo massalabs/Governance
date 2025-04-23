52366aeefaafa9cf2f7c46a8879ad6980af82f9e
ben888 <br@massa.net>
2025-04-23 14:36:18 +0700

    refactor: remove unused governance data hook from useInitApp
    - Eliminated the unused `useGovernanceData` hook from the `useInitApp` function to simplify the code and improve performance.
    - Updated the dependency array in the `useEffect` hook accordingly, ensuring it only tracks `connectedAccount`.

--- front/src/hooks/useInitApp.ts
+++ front/src/hooks/useInitApp.ts
@@ -2,12 +2,10 @@ import { toast, useAccountStore } from "@massalabs/react-ui-kit";
 import { useBalanceRefresh } from "../react-queries/useBalanceRefresh";
 import { useEffect } from "react";
 import { useContractStore } from "@/store/useContractStore";
-import { useGovernanceData } from "../react-queries/useGovernanceData";
 import useAccountSync from "./useAccountSync";
 import { networkName } from "@/config";
 
 export function useInitApp() {
-    const { refresh } = useGovernanceData();
     const { connectedAccount, network } = useAccountStore();
     const { initializeAllContracts, initializePublicContracts } = useContractStore();
 
@@ -21,7 +19,7 @@ export function useInitApp() {
             initializePublicContracts()
         }
 
-    }, [connectedAccount, refresh]);
+    }, [connectedAccount]);
 
     useEffect(() => {
         if (network) {
