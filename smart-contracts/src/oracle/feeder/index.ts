8e3ce91c06ae7e048c4ae8a86166715d55e5d93b
ben888 <br@massa.net>
2025-08-14 12:09:40 +0700

    remove buildnet feed

--- smart-contracts/src/oracle/feeder/index.ts
+++ smart-contracts/src/oracle/feeder/index.ts
@@ -1,10 +1,10 @@
 /* eslint-disable camelcase */
 import { log, logWithDivider } from './log';
-import { Account, NetworkName, Web3Provider } from "@massalabs/massa-web3";
-import { contracts } from "../../config";
-import { Oracle } from "../wrapper/Oracle";
-import { Feeder } from "./feeder";
-import { AlertsService } from "./alerts";
+import { Account, NetworkName, Web3Provider } from '@massalabs/massa-web3';
+import { contracts } from '../../config';
+import { Oracle } from '../wrapper/Oracle';
+import { Feeder } from './feeder';
+import { AlertsService } from './alerts';
 import { getStakers } from './helpers';
 import dotenv from 'dotenv';
 
@@ -23,11 +23,14 @@ async function main() {
   const accountBuildnet = await Account.fromEnv('PRIVATE_KEY_BUILDNET');
   const providerBuildnet = Web3Provider.buildnet(accountBuildnet);
 
-
   // TODO - Alerte Balance if lower than 1000
   const balanceMainnet = await providerMainnet.balance();
   if (balanceMainnet < 1000n) {
-    await alertsService.triggerAlert('balance-low', 'Balance of technical address is lower than 1000', 'critical');
+    await alertsService.triggerAlert(
+      'balance-low',
+      'Balance of technical address is lower than 1000',
+      'critical',
+    );
   }
 
   const mainnetFeeder = new Feeder(
@@ -45,17 +48,22 @@ async function main() {
   );
 
   // log contracts addresses
-  log.info(`Mainnet Oracle Contract Address: ${contracts[NetworkName.Mainnet].oracle}`);
-  log.info(`Buildnet Oracle Contract Address: ${contracts[NetworkName.Buildnet].oracle}`);
+  log.info(
+    `Mainnet Oracle Contract Address: ${contracts[NetworkName.Mainnet].oracle}`,
+  );
+  log.info(
+    `Buildnet Oracle Contract Address: ${
+      contracts[NetworkName.Buildnet].oracle
+    }`,
+  );
 
   const stakers = await getStakers(providerMainnet);
 
   // TODO: If mainnet feeder fails, buildnet is not executed
   await mainnetFeeder.feed(stakers);
-  await buildnetFeeder.feed(stakers);
+  // await buildnetFeeder.feed(stakers);
 
   logWithDivider('All feeders finished successfully', 'success');
 }
 
 main();
-
